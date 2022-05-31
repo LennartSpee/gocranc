3e80d55fd7aa313d243e4e0cb56091523babb664
Tito Romao <titoromao@hotmail.com>
2022-05-31 12:56:01 +0100

    offersave

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -4,7 +4,7 @@ class Offer < ApplicationRecord
   # geocoded_by :location
   # after_validation :geocode, if: :will_save_change_to_location?
   # has_one_attached :photo
-  # validates :price, presence: true
+  # validates :price,  presence: true
   validates :title, presence: true
   validates :description, presence: true
   validates :duration, presence: true
