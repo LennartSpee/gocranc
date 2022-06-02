b347b51839227702d35313f71e817866aec0096b
Lennart Spee <lennart@digitallokal.de>
2022-06-02 12:58:20 +0100

    different stuff changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'about', to: 'pages#about'
+  get 'faq', to: 'pages#faq'
+  get 'guides', to: 'pages#guides'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :profiles, only: [:show, :update, :create, :new]
