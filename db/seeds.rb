5110d031c9da5102e5f181b964eb1eb7f42ba75d
Lennart Spee <lennart@digitallokal.de>
2022-06-06 18:07:56 +0100

    seeds for offers done

--- db/seeds.rb
+++ db/seeds.rb
@@ -81,7 +81,7 @@ sport7.save
 # Offers
 
 offer1 = Offer.new(
-  title: 'Skiing',
+  title: 'Skitour Davos',
   price: '50',
   duration: '30',
   description: 'Learn how to stand up on a bord and catch your first waves',
@@ -94,35 +94,52 @@ file = URI.open('https://cdn.betterwalls.com/pim/pr/WP/30016334/photo-wallpaper-
 offer1.photo.attach(io: file, filename: 'ski.jpg', content_type: 'image/jpg')
 offer1.save
 
-# offer2 = Offer.new(title: "Climbing")
-# file = URI.open('https://img.redbull.com/images/c_crop,w_5122,h_2561,x_0,y_2956,f_auto,q_auto/c_scale,w_1200/redbullcom/2019/06/03/83ddcec2-b607-4def-a965-abb0b94fa412/how-to-take-indoor-climbing-outdoors')
-# offer2.photo.attach(io: file, filename: 'climb.jpg', content_type: 'image/jpg')
-# offer2.save
+offer2 = Offer.new(
+  title: 'Climbing in Brazil',
+  price: '50',
+  duration: '30',
+  description: 'Control your body, control your mind',
+  location: 'Rio de Janeiro',
+  latitude: 38.722028,
+  longitude: -9.139139,
+  user: User.second
+)
 
-# offer3 = Offer.new(title: "Biking")
-# file = URI.open('https://www.canyon.com/dw/image/v2/BCML_PRD/on/demandware.static/-/Library-Sites-canyon-shared/default/dw87df4949/images/plp/Mountain/Sender/ridestyle-downhill-canyon-sender-01.jpg?sw=1920')
-# offer3.photo.attach(io: file, filename: 'biking.jpg', content_type: 'image/jpg')
-# offer3.save
+file = URI.open('https://img.redbull.com/images/c_crop,w_5122,h_2561,x_0,y_2956,f_auto,q_auto/c_scale,w_1200/redbullcom/2019/06/03/83ddcec2-b607-4def-a965-abb0b94fa412/how-to-take-indoor-climbing-outdoors')
+offer2.photo.attach(io: file, filename: 'climb.jpg', content_type: 'image/jpg')
+offer2.save
 
-# offer4 = Offer.new(title: "Flying")
-# file = URI.open('http://upload.wikimedia.org/wikipedia/commons/9/9b/Ocean_Wingsuit_Formation_%286366966219%29.jpg')
-# offer4.photo.attach(io: file, filename: 'flying.jpg', content_type: 'image/jpg')
-# offer4.save
 
-# offer5 = Offer.new(title: "Surfing")
-# file = URI.open('https://www.boardsportsource.com/wp-content/uploads/2021/07/Mikey-February-by-Alan-Van-Gysen.jpg')
-# offer5.photo.attach(io: file, filename: 'surfing.jpg', content_type: 'image/jpg')
-# offer5.save
+offer3 = Offer.new(
+  title: 'Climbing in Brazil',
+  price: '50',
+  duration: '30',
+  description: 'Control your body, control your mind',
+  location: 'Rio de Janeiro',
+  latitude: 38.722028,
+  longitude: -9.139139,
+  user: User.second
+)
 
-# offer6 = Offer.new(title: "Diving")
-# file = URI.open('https://www.seabookings.com/blog/wp-content/uploads/2016/06/Coasteering-Red-Bull.jpg')
-# offer6.photo.attach(io: file, filename: 'diving.jpg', content_type: 'image/jpg')
-# offer6.save
+file = URI.open('https://www.canyon.com/dw/image/v2/BCML_PRD/on/demandware.static/-/Library-Sites-canyon-shared/default/dw87df4949/images/plp/Mountain/Sender/ridestyle-downhill-canyon-sender-01.jpg?sw=1920')
+offer3.photo.attach(io: file, filename: 'biking.jpg', content_type: 'image/jpg')
+offer3.save
 
-# offer7 = Offer.new(title: "Moto")
-# file = URI.open('https://racemotor.com.br/wp-content/uploads/2021/09/SI202109190758.jpg')
-# offer7.photo.attach(io: file, filename: 'moto.jpg', content_type: 'image/jpg')
-# offer7.save
+offer4 = Offer.new(title: 'Biking in Portugal', price: '50', duration: '30', description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,', location: 'Monsanto', latitude: 39.46203, longitude: -8.7118, user: User.third)
+file = URI.open('http://upload.wikimedia.org/wikipedia/commons/9/9b/Ocean_Wingsuit_Formation_%286366966219%29.jpg')
+offer4.photo.attach(io: file, filename: 'flying.jpg', content_type: 'image/jpg')
+offer4.save
+
+offer5 = Offer.new(title: 'Surfing in France', price: '50', duration: '30', description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,', location: 'Lisbon', latitude: 38.722028, longitude: -10.139139, user: User.third)
+file = URI.open('https://www.boardsportsource.com/wp-content/uploads/2021/07/Mikey-February-by-Alan-Van-Gysen.jpg')
+offer5.photo.attach(io: file, filename: 'surfing.jpg', content_type: 'image/jpg')
+offer5.save
+
+offer6 = Offer.new(title: 'Moto', price: '50', duration: '30', description: 'Play like a pro', location: 'Lisbon', latitude: 38.722028, longitude: -9.139139, user: User.all.sample)
+file = URI.open('https://racemotor.com.br/wp-content/uploads/2021/09/SI202109190758.jpg')
+offer6.photo.attach(io: file, filename: 'diving.jpg', content_type: 'image/jpg')
+offer6.save
+
 
 p "sports with pics"
 
@@ -135,29 +152,29 @@ p "sports with pics"
 #   end
 # end
 
-offer = Offer.create!(
-  title: 'Skiing',
-  price: '50',
-  duration: '30',
-  description: 'Learn how to stand up on a bord and catch your first waves',
-  location: 'Lisbon',
-  latitude: 38.722028,
-  longitude: -9.139139,
-  user: User.first
-)
+# offer = Offer.create!(
+#   title: 'Skiing',
+#   price: '50',
+#   duration: '30',
+#   description: 'Learn how to stand up on a bord and catch your first waves',
+#   location: 'Lisbon',
+#   latitude: 38.722028,
+#   longitude: -9.139139,
+#   user: User.first
+# )
 
-OfferSport.create(offer: offer, sport: Sport.find_by(name: "Skiing"))
+# OfferSport.create(offer: offer, sport: Sport.find_by(name: "Skiing"))
 
-offer = Offer.create!(
-  title: 'Climbing',
-  price: '50',
-  duration: '30',
-  description: 'Control your body, control your mind',
-  location: 'Lisbon',
-  latitude: 38.722028,
-  longitude: -9.139139,
-  user: User.second
-)
+# offer = Offer.create!(
+#   title: 'Climbing',
+#   price: '50',
+#   duration: '30',
+#   description: 'Control your body, control your mind',
+#   location: 'Lisbon',
+#   latitude: 38.722028,
+#   longitude: -9.139139,
+#   user: User.second
+# )
 
 OfferSport.create(offer: offer, sport: Sport.find_by(name: "Climbing"))
 offer = Offer.create!(title: 'Biking', price: '50', duration: '30', description: 'Lord of the Rings', location: 'Lisbon', latitude: 38.722028, longitude: -10.139139, user: User.third)
